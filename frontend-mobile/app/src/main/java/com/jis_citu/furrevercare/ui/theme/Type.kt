565f10a1a0aeceff8fba96b77969f82d867acf1f
Isaac <quiloisaac@gmail.com>
2025-04-10 06:34:56 +0800

    Init Commit of Mobile UI

---
+++ frontend-mobile/app/src/main/java/com/jis_citu/furrevercare/ui/theme/Type.kt
@@ -0,0 +1,7 @@
+package com.jis_citu.furrevercare.ui.theme
+
+import androidx.compose.material3.Typography
+
+val Typography = Typography(
+    // You can customize heading, body, labels here if needed
+)
